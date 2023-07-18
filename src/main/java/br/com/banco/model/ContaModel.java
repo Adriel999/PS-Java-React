c7a25ae2a0a86e800cc94cd8dd1d1014dd8cd014
Adriel Gomes <adriel_gomes999@outlook.com>
2023-07-18 01:45:26 -0300

    Correção do ponteiro de tabela da entidade Conta e correção do local da classe BancoApplication.

--- src/main/java/br/com/banco/model/ContaModel.java
+++ src/main/java/br/com/banco/model/ContaModel.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "transferencia")
+@Table(name = "conta")
 public class ContaModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
